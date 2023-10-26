67c09fde952a1b0524d28b143f2d05d215cb876d
abell246 <abell246@uwo.ca>
2023-10-26 00:09:48 -0400

    Aidan Testing
    I didn't change anything except adding a testing text file that does nothing

--- Images/Assets/Wizard(28 frames between)(118px apart)/README.txt
+++
@@ -1,3 +0,0 @@
-60 FPS:
-Wizard_Default - 28
-Wizard_Idle - 28(28-56 frames)
--- Particles/enemy_death_particle.gd
+++
@@ -1,8 +0,0 @@
-extends GPUParticles2D
-
-func _ready():
-	self.emitting = true
-	
-func _process(_delta):
-	if(!self.emitting):
-		queue_free()
