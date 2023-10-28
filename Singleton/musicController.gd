99e271d29c95b35e63b1c4625003b529beb20b45
MattCraft9 <matthewkleisath@gmail.com>
2023-10-28 01:12:57 -0400

    main theme plays

--- Singleton/musicController.gd
+++ Singleton/musicController.gd
@@ -1,12 +1,14 @@
 extends Node
 
-#var main_music = load("res://Main_Theme_v1.mp3")
+var main_music = load("res://Sounds/Main Theme Master.wav")
 var start_sound = load("res://Sounds/Start Sound.wav")
 var hover_sound = load("res://Sounds/Hover Over Button.wav")
 
-#func play_theme():
-	#$"main-theme".stream = main_music
-	#$"main-theme".play()
+func play_theme():
+	$"Main-Theme".stream = main_music
+	$"Main-Theme".play()
+	
+
 	
 func play_startFX():
 	$"Start-SFX".stream = start_sound
@@ -14,3 +16,7 @@ func play_startFX():
 func play_hoverFX():
 	$"Hover-SFX".stream = hover_sound
 	$"Hover-SFX".play()
+
+
+func _on_main_theme_finished():
+	play_theme()
