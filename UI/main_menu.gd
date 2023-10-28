3be3611ac50c551bfaf34fd207451084a42f9899
Munchie <asm2002@live.ca>
2023-10-28 01:30:04 -0400

    Play button works

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
 
-#func _on_play_pressed():
-	#MusicController.play_startFX()
-	#await get_tree().create_timer(0.85).timeout
-	#get_tree().change_scene_to_file("res://world.tscn") #***change later when cutscene is done
+func _on_play_pressed():
+	MusicController.play_startFX()
+	await get_tree().create_timer(0.85).timeout
+	Transition.changeScene("res://Levels/cutscene_test.tscn") #***change later when cutscene is done
 
 
 func _on_settings_pressed():
