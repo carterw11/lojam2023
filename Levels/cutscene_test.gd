4cbf7a4b09bf1b254f8832edf4c4e2336def94d0
MattCraft9 <matthewkleisath@gmail.com>
2023-10-28 00:08:16 -0400

    cutscene

--- Levels/cutscene_test.gd
+++ Levels/cutscene_test.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
 @onready var player = $Player
+@onready var bean = $bean
 @onready var playerWalkInTimer = $PlayerWalkIn
 @onready var beanThrowTimer = $BeanThrowTime
+@onready var wizardTalkTime = $WizardTalkTime
 @onready var wizardFlyOffTimer = $WizardFlyOffscreen
 @onready var initialTimer = $ReadyTimer
 @onready var wizard = $Wizard
 @onready var beanStalk = $Beanstalk
 var wizardIsFlying : bool  = false
 @onready var playerWalkOutTimer = $PlayerWalkOut
+
 
 func _physics_process(_delta):
 	if(wizardIsFlying):
@@ -17,6 +20,9 @@ func _physics_process(_delta):
 
 func _ready():
 	initialTimer.start()
+	$PlayerText.visible = !$PlayerText.visible
+	$WizardText.visible = !$WizardText.visible
+	bean.visible = !bean.visible
 
 func _on_ready_timer_timeout():
 	player.velocity.x = 600
@@ -29,17 +35,25 @@ func _on_player_walk_in_timeout():
 	player.velocity.x = 0
 	player.sprite.stop()
 	player.sprite.play("Idle")
+	$PlayerText.show()
 	beanThrowTimer.start()
 
-
-func _on_bean_throw_time_timeout():
+func _on_wizard_talk_time_timeout():
+	$WizardText.hide()
 	wizardIsFlying = true
 	wizardFlyOffTimer.start()
+
+func _on_bean_throw_time_timeout():
+	$PlayerText.hide()
+	$WizardText.show()
+	bean.visible = !bean.visible
+	wizardTalkTime.start()
 
 
 func _on_wizard_fly_offscreen_timeout():
 	wizardIsFlying = false
 	player.velocity.x = 600
+	bean.visible = !bean.visible
 	player.sprite.stop()
 	player.sprite.play("Run")
 	playerWalkOutTimer.start()
